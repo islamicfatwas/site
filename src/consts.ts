fc1f7c27cc791af06da71042d6a43230e3c44518
islamicfatwas <aliftaid@gmail.com>
2024-06-24 19:40:37 +0700

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const site = "https://example.com";
+export const site = "https://islamicfatwas.github.io/site/";
 export const siteTitle = "Astro Blog Starter";
 export const siteThemeColor = "#27AE60";
 export const pagination = {
